3ae0e03a8725195a3aa2dfc31bec230179f2048b
Junior7 <giftmomanyi97@gmail.com>
2025-09-04 07:26:22 +0300

    Changes Taken

--- Introduction_to_Django/LibraryProject/bookshelf/models.py
+++ Introduction_to_Django/LibraryProject/bookshelf/models.py
@@ -1,7 +1,13 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    """Table for Author Names"""
+    name = models.CharField(max_length=200)
+
+
+
 class Book(models.Model):
     title = models.CharField(max_length=200)
-    author = models.CharField(max_length=100)
+    author = models.ForeignKey(Author, on_delete=models.CASCADE)
     publication_year = models.IntegerField()
