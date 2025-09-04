091ba36455ef6507163a8cbb352003eea49e847d
Junior7 <giftmomanyi97@gmail.com>
2025-09-04 07:36:50 +0300

    fix(Models.py)

--- LibraryProject/relationship_app/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from .models import AuthorModel, BookModel, LibrarianModel, LibraryModel
-
-# Register your models here.
-admin.site.register(AuthorModel)
-admin.site.register(BookModel)
-admin.site.register(LibraryModel)
-admin.site.register(LibrarianModel)
