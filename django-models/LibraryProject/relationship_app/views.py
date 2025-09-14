aad333eb0de73735e8098099c9284092c4e59962
Junior7 <giftmomanyi97@gmail.com>
2025-09-14 15:45:24 +0300

    feat: add role-based views for members, librarians, and admins with access control

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -1,14 +1,59 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import Book
-from .models import Library
+from .models import Author, Book, Librarian, Library
 from django.views.generic.detail import DetailView
+from django.contrib.auth.decorators import user_passes_test
 
 # User registration
 from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
+# --- Role check functions ---
+def is_member(user):
+    return user.is_authenticated and user.groups.filter(name="Members").exists()
+
+def is_librarian(user):
+    return user.is_authenticated and user.groups.filter(name="Librarians").exists()
+
+def is_admin(user):
+    return user.is_authenticated and user.is_staff  # or check a "Admins" group if you created one
+
+
+# --- Member view ---
+@user_passes_test(is_member, login_url="/not-authorized/")
+def member_display(request):
+    context = {
+        "librarians": Librarian.objects.all(),
+        "libraries": Library.objects.all(),
+        "books": Book.objects.all(),
+        "authors": Author.objects.all(),
+    }
+    return render(request, "relationship_app/member_view.html", context)
+
+
+# --- Librarian view ---
+@user_passes_test(is_librarian, login_url="/not-authorized/")
+def librarian_view(request):
+    context = {
+        "librarians": Librarian.objects.all(),
+        "libraries": Library.objects.all(),
+        "books": Book.objects.all(),
+        "authors": Author.objects.all(),
+    }
+    return render(request, "relationship_app/librarian_view.html", context)
+
+
+# --- Admin view ---
+@user_passes_test(is_admin, login_url="/not-authorized/")
+def admin_view(request):
+    context = {
+        "librarians": Librarian.objects.all(),
+        "libraries": Library.objects.all(),
+        "books": Book.objects.all(),
+        "authors": Author.objects.all(),
+    }
+    return render(request, "relationship_app/admin_view.html", context)
 
 # Create your views here.
 class SignUpView(CreateView):
