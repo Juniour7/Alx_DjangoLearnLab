016d8cb0a7482122189d3f2399e46294a2779c88
Junior7 <giftmomanyi97@gmail.com>
2025-09-16 08:01:57 +0300

    feat: add CustomUser model with date of birth and profile photo fields

--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,6 +1,11 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 # Create your models here.
+class CustomUser(AbstractUser):
+    date_of_birth = models.DateField(null=True, blank=True)
+    profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
 class Author(models.Model):
     """Table for Author Names"""
     name = models.CharField(max_length=200)
