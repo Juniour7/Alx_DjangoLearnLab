01c164bc2c8033d253f690b915636c6f30287dd7
Junior7 <giftmomanyi97@gmail.com>
2025-09-16 08:43:24 +0300

    feat: implement CustomUser model with custom user manager and update settings

--- advanced_features_and_security/LibraryProject/bookshelf/admin.py
+++ advanced_features_and_security/LibraryProject/bookshelf/admin.py
@@ -1,11 +1,16 @@
 from django.contrib import admin
-from .models import Book, Author
+from .models import Book, Author, CustomUser
 
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title','author', 'publication_year')
     list_filter = ('title','author', 'publication_year')
     search_fields = ('title', 'author', 'publication_year')
 
+class ModelAdmin(admin.ModelAdmin):
+    list_display = ('username', 'email', 'date_of_birth')
+    list_filter = ('username', 'email', 'date_of_birth')
+
 # Register your models here.
 admin.site.register(Book, BookAdmin)
 admin.site.register(Author)
+admin.site.register(CustomUser, ModelAdmin)
--- advanced_features_and_security/LibraryProject/bookshelf/models.py
+++ advanced_features_and_security/LibraryProject/bookshelf/models.py
@@ -1,10 +1,28 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractUser, BaseUserManager
 
 # Create your models here.
 class CustomUser(AbstractUser):
     date_of_birth = models.DateField(null=True, blank=True)
     profile_photo = models.ImageField(upload_to='profile_photos/', null=True, blank=True)
+
+class CustomUserManager(BaseUserManager):
+    def create_user(self, email, password, date_of_birth=None, profile_photo=None):
+        if not email:
+            raise ValueError('The Email field must be set')
+        if not password:
+            raise ValueError('The Password field must be set')
+        email = self.normalize_email(email)
+        user = self.model(email=email, date_of_birth=date_of_birth, profile_photo=profile_photo)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_superuser(self, email, password, date_of_birth=None, profile_photo=None):
+        user = self.create_user(email, password, date_of_birth, profile_photo)
+        user.is_superuser = True
+        user.save(using=self._db)
+        return user
 
 class Author(models.Model):
     """Table for Author Names"""
