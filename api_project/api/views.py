06ff783b68d5d6ed5c733cff698d48e313ff6eb9
Junior7 <giftmomanyi97@gmail.com>
2025-09-21 10:53:20 +0300

    feat: Book List API created and tested

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from .serializers import BookViewSerializer
+from rest_framework import generics
+from .models import Book
+
 
 # Create your views here.
+class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookViewSerializer
