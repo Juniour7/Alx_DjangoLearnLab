9a3d2d01d912805abd21770a53eb6b90f68b908d
Junior7 <giftmomanyi97@gmail.com>
2025-10-05 23:03:59 +0300

    feat:trial

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import AllowAny, IsAuthenticated
+from django.contrib.auth import login, logout
+
+
+from .forms import RegisterForm
 
 # Create your views here.
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def register_view(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('profile')
+    
+    else:
+        form = RegisterForm()
+
+    return render(request, 'a')
