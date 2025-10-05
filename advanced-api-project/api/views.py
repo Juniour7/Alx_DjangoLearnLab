1ba6efc1de890f15405555dd798e8e3bafd84c73
Junior7 <giftmomanyi97@gmail.com>
2025-10-05 19:51:38 +0300

    fix:permisions for views updated

--- advanced-api-project/api/views.py
+++ advanced-api-project/api/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 from rest_framework import generics
-from rest_framework.permissions import IsAuthenticated, AllowAny
+from rest_framework.permissions import IsAuthenticatedOrReadOnly, IsAuthenticated
 
 from .models import Author, Book
 from .serializers import BookSerializer, AuthorSerializer
@@ -9,13 +9,14 @@ from .serializers import BookSerializer, AuthorSerializer
 class BookListView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-    permission_classes = [AllowAny]
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 
 class BookDetailView(generics.DetailAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
     search_fields = ['title', 'publication_year', 'author']
+    permission_classes = [IsAuthenticatedOrReadOnly]
 
 class BookCreateView(generics.CreateAPIView):
     queryset = Book.objects.all()
