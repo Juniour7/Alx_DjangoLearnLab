4613ab6decd44757bb1a2262903dfb4c1caac316
Junior7 <giftmomanyi97@gmail.com>
2025-10-05 19:50:17 +0300

    fix:ur route names updated to meet requirements

--- advanced-api-project/api/urls.py
+++ advanced-api-project/api/urls.py
@@ -4,7 +4,7 @@ from . import views
 urlpatterns = [
     path('books/', views.BookListView, name='book-list'),
     path('books/<int:pk>/', views.BookDetailView, name='book_detail-view'),
-    path('book-new/', views.BookCreateView, name='create_book'),
-    path('book-update/', views.BookUpdateView, name='update_book'),
-    path('book-delete/', views.BookDeleteView, name='delete_book'),
+    path('books/create/', views.BookCreateView, name='create_book'),
+    path('books/update/', views.BookUpdateView, name='update_book'),
+    path('books/delete/', views.BookDeleteView, name='delete_book'),
 ]
