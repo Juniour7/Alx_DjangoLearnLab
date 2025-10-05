7868bf39b9fb92577aa081a5d73ee8a54c5457bb
Junior7 <giftmomanyi97@gmail.com>
2025-10-05 23:16:07 +0300

    feat:urls set up

--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from django.contrib.auth import login, logout
+from django.contrib.auth.forms import AuthenticationForm
 
 
 from .forms import RegisterForm
@@ -20,4 +21,23 @@ def register_view(request):
     else:
         form = RegisterForm()
 
-    return render(request, 'a')
+    return render(request, 'blog/register.html', {'form': form})
+
+@api_view(['POST'])
+@permission_classes([AllowAny])
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(data=request.POST)
+        if form.is_valid():
+            user = form.get_user()
+            login(request, user)
+            return redirect('profile')
+    else:
+        form = AuthenticationForm()
+    return render(request, 'blog/login.html', {'form': form})
+
+@api_view(['POST'])
+@permission_classes([IsAuthenticated])
+def logout_view(request):
+    logout(request)
+    return render(request, 'blog/logout.html')
