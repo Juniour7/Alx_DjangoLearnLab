b92c382d0d0aba59d95a747720bedc23b5ce3150
Junior7 <giftmomanyi97@gmail.com>
2025-10-10 12:02:51 +0300

    fix:url set naming

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -7,9 +7,9 @@ urlpatterns = [
     path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('post/<int:pk>/update/', views.PostUpdateView.as_view(), name='post-update'),
     path('post/<int:pk>/delete/', views.PostDeleteView.as_view(), name='post-delete'),
-    path('post/<int:pk>/comment/new/', views.CommentCreateView.as_view(), name='add-comment'),
-    path('post/<int:pk>/comment/update/', views.CommentUpdateView.as_view(), name='update-comment'),
-    path('post/<int:pk>/comment/delete/', views.CommentDeleteView.as_view(), name='delete-comment'),
+    path('comment/<int:pk>/new/', views.CommentCreateView.as_view(), name='add-comment'),
+    path('comment/<int:pk>/update/', views.CommentUpdateView.as_view(), name='update-comment'),
+    path('comment/<int:pk>/delete/', views.CommentDeleteView.as_view(), name='delete-comment'),
 
 
     path('home/', views.home_view, name='home'),
